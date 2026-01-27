4a927787ce4dd23e0df7a1a99f945c5dde1796b8
codemasterzhao <zhao.haib@northeastern.com>
2026-01-26 17:51:03 -0800

    train symmetry script

---
+++ pretrain.sh
@@ -0,0 +1,2 @@
+  python train.py --config-name=train_maniflow_pointcloud_canonical_workspace
+  task_name=nut_assembly_d0 n_demo=100
--- run_rl_training.sh
+++
@@ -1,5 +0,0 @@
-  python train_maniflow_rl.py \                                                                                                                                                 
-      --config-path=equi_diffpo/config \                                                                                                                                        
-      --config-name=train_maniflow_pointcloud_rl \                                                                                                                              
-      task_name=nut_assembly_d0 \                                                                                                                                               
-      rl_training.num_envs=16   
