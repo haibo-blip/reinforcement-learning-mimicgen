ccc3200a9b1ac170b041df120c6f446fd526103b
codemasterzhao <zhao.haib@northeastern.com>
2026-01-30 15:20:37 -0800

    training stack_three

--- pretrain.sh
+++ pretrain.sh
@@ -1,1 +1,1 @@
-  python train.py --config-name=train_maniflow_pointcloud_canonical_workspace task_name=nut_assembly_d0 n_demo=100
+  python train.py --config-name=train_maniflow_pointcloud_canonical_workspace task_name=stack_three_d1 n_demo=100
