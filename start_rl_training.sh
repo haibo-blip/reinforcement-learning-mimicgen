170d3ad3e86d0d3574d8ad0bd6d82d819cbb42cd
codemasterzhao <zhao.haib@northeastern.com>
2026-02-02 13:32:22 -0800

    equi rl

--- start_rl_training.sh
+++ start_rl_training.sh
@@ -3,4 +3,4 @@ python train_maniflow_rl.py \
     --config-name=train_maniflow_pointcloud_rl \
     task_name=stack_three_d1 \
     rl_training.num_envs=16\
-    policy.checkpoint="data/outputs/2026.01.31/01.47.07_train_maniflow_pointcloud_stack_three_d1/checkpoints/1.ckpt"
+    policy.checkpoint="data/outputs/2026.01.31/02.37.04_train_maniflow_pointcloud_canonical_stack_three_d1/checkpoints/1.ckpt"
