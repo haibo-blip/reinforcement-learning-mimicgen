329b472305f7258fa605d5687dc55ac3d1324a2a
codemasterzhao <zhao.haib@northeastern.com>
2026-02-02 13:16:25 -0800

    fix checkpoint

--- start_rl_training.sh
+++ start_rl_training.sh
@@ -1,6 +1,1 @@
-python train_maniflow_rl.py \
-    --config-path=equi_diffpo/config \
-    --config-name=train_maniflow_pointcloud_rl \
-    task_name=nut_assembly_d0 \
-    rl_training.num_envs=16 \
-    policy.checkpoint="data/outputs/2026.01.31/01.47.07_train_maniflow_pointcloud_stack_three_d1/checkpoints/epoch=0170-test_mean_score=0.100.ckpt"
+python train_maniflow_rl.py --config-path=equi_diffpo/config --config-name=train_maniflow_pointcloud_rl task_name=nut_assembly_d0 rl_training.num_envs=16 policy.checkpoint="data/outputs/2026.01.31/01.47.07_train_maniflow_pointcloud_stack_three_d1/checkpoints/epoch=0170-test_mean_score=0.100.ckpt"
