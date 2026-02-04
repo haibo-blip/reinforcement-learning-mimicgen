0be7c77a674c4b584ce85d7b63f6b232aa43d940
codemasterzhao <zhao.haib@northeastern.com>
2026-02-04 10:55:08 -0800

    stack_d1 pretrain and rl

--- start_rl_training.sh
+++ start_rl_training.sh
@@ -1,6 +1,6 @@
 python train_maniflow_rl.py \
     --config-path=equi_diffpo/config \
     --config-name=train_maniflow_pointcloud_rl \
-    task_name=stack_three_d1 \
+    task_name=stack_d1 \
     rl_training.num_envs=16\
     policy.checkpoint="data/outputs/2026.02.03/20.00.27_train_maniflow_pointcloud_stack_d1/checkpoints/epoch0300-test_mean_score0.800.ckpt"
