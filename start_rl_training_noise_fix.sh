023fc722b925c7ca571dcc60fbd6effebcda559f
codemasterzhao <zhao.haib@northeastern.com>
2026-02-04 09:51:02 -0800

    fix noise script

---
+++ start_rl_training_noise_fix.sh
@@ -0,0 +1,9 @@
+python train_maniflow_rl.py \
+    --config-path=equi_diffpo/config \
+    --config-name=train_maniflow_pointcloud_rl \
+    task_name=stack_three_d1 \
+    rl_training.num_envs=16\
+    policy.checkpoint="data/outputs/2026.02.03/01.15.54_train_maniflow_pointcloud_stack_three_d1/checkpoints/1.ckpt"\
+    policy.noise_level=0.3\
+    policy.noise_anneal=false
+
